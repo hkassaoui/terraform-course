f0586128fa2ff4f2e66c0abee35754307b297bdf
Hichem Kassaoui <hichem.kassaoui@outlook.com>
2024-11-05 14:33:13 -0500

    variables changes

--- 02_WebApp/main.tf
+++ 02_WebApp/main.tf
@@ -1,20 +1,15 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-}
-
 resource "azurerm_service_plan" "plan" {
   name                = var.app_service_plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   os_type             = "Linux" # "Windows"
   sku_name            = "S1"
 }
 
 resource "azurerm_linux_web_app" "app" {
   name                = var.app_service_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
